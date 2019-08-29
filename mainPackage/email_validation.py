7b6b5bce9ad9b3d1c3ad6899faeff58dc653f99b
David <davixrgithub@gmail.com>
2019-08-29 13:36:20 +0200

    Some small name changes

--- mainPackage/email_validation.py
+++ mainPackage/email_validation.py
@@ -7,9 +7,10 @@ import re
 
 def email_analyze(lines):
     emails = []
-    find_pattern = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
+    # The following pattern matches the RFC 5322 criteria:
+    pattern = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
     for line in lines:
-        line_emails = re.findall(find_pattern, line)
+        line_emails = re.findall(pattern, line)
         for email in line_emails:
             emails.append(email)
     return emails

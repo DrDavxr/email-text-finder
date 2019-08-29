6da86759bace0fc1c059cabd3040a889fae5c108
David <davixrgithub@gmail.com>
2019-08-29 14:13:05 +0200

    Changes in the code related with optimization.

--- mainPackage/file_manipulation.py
+++ mainPackage/file_manipulation.py
@@ -2,24 +2,25 @@
 This module contains all the code related with the manipulation of file(s).
 '''
 import sys
+from mainPackage import email_validation
 
 
 class File():
 
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self, filename):
+        self.filename = filename
 
     def __str__(self):
-        return "Opening file '{file}' ... \n".format(file=self.file_name)
+        return "Opening file '{file}' ... \n".format(file=self.filename)
 
-    def open_file(self):
+    def file_processing(self, emails):
         '''
         Function that opens an specific file.
         '''
         try:
-            with open(self.file_name, 'r') as f:
-                self.lines = f.readlines()  # list containing all the lines.
-                f.close()
-                return self.lines  # Returns a list with all the lines.
+            with open(self.filename, 'r') as f:
+                for line in f:
+                    emails = email_validation.email_analyze(line, emails)
+            return emails
         except FileNotFoundError:
             sys.exit("FILE WAS NOT SELECTED OR FOUND!")
